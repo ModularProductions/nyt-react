e43aeb52515838658712184b69463fae2475fb0b
Matt <777mth@gmail.com>
2018-04-28 02:52:11 -0400

    Building for production

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React, Dude</h2>
+          <h2>Welcome to React, Dude. You're updated in Heroku.</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
